1ea4f14b115c6c95558ab4b28c3ad3de4479afcf
AnviTy <anvee.tyagi@gmail.com>
2026-07-20 19:53:26 +0530

    Initialize React frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🛡️ SentinelX</h1>
+
+      <h2>Cloud Hosted SOAR Platform</h2>
+
+      <p>Frontend Status : 🟢 Running</p>
+    </div>
+  );
+}
+
+export default App;
